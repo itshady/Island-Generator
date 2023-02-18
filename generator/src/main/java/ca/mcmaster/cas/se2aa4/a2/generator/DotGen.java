634a0e882f45370ec78ff5b934e262ad18b894fb
richardl2003 <richardliy03@gmail.com>
2023-02-18 17:17:44 -0500

    Added support for command line args to select the mesh type, still need to do default args

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/DotGen.java
@@ -4,7 +4,15 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 public class DotGen {
 
+    private String meshType;
+
+    // Set this one as the default
     public Structs.Mesh generate() {
-        return new MeshFactory().generate("square_regular");
+        return new MeshFactory().generate("irregular");
+    }
+
+    // Overloading the generate method
+    public Structs.Mesh generate(String meshType) {
+        return new MeshFactory().generate(meshType);
     }
 }
--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MeshFactory.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MeshFactory.java
@@ -4,11 +4,11 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 public class MeshFactory {
     public Structs.Mesh generate(String arg) throws IllegalArgumentException {
-        if (arg == "square_regular" || arg == "regular") {
+        if (arg.equals("square_regular") || arg.equals("regular")) {
             return new SquareRegularMesh().generate();
-        } else if (arg == "hex_regular") {
+        } else if (arg.equals("hex_regular")) {
             return new HexRegularMesh().generate();
-        } else if (arg == "irregular") {
+        } else if (arg.equals("irregular")) {
             return new IrregularMesh().generate();
         }
         throw new IllegalArgumentException("Use --help if you want to know what you can pass in.");
