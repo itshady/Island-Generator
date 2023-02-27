6934e0e662aa5bd5663017bb27938447b41c65fe
cyrussamante <cyruss_amante@yahoo.ca>
2023-02-27 13:22:19 -0500

    Added comment for NormalMode.

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/Modes/NormalMode.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/Modes/NormalMode.java
@@ -4,6 +4,10 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 import java.awt.*;
 
+/**
+ * Renders the vertices, segments and polygons according to default colour, thickness and alpha values.
+ */
+
 public class NormalMode extends VisualMode{
     public void render(Structs.Mesh aMesh, Graphics2D canvas) {
         super.render(aMesh,canvas);
