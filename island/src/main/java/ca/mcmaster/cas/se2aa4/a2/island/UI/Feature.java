a30af3cf642f1572b09b29db4cac38fb850af445
richardl2003 <richardliy03@gmail.com>
2023-03-14 19:50:55 -0400

    Pt.2 added the new specification support to select shape properly

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/UI/Feature.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas.se2aa4.a2.island.UI;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Feature {
+
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/UI/FeatureRunner.java
@@ -0,0 +1,9 @@
+package ca.mcmaster.cas.se2aa4.a2.island.UI;
+
+import ca.mcmaster.cas.se2aa4.a2.island.Containers.Island;
+
+public interface FeatureRunner {
+
+    void process(Island island, Configuration config);
+
+}
