f086916e06f94df94e3be18f232842fd2e1089ad
richardl2003 <richardliy03@gmail.com>
2023-03-20 14:18:18 -0400

    F36: WIP for the american whittaker diagram

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Biomes/AmericaProfile.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Biomes/AmericaProfile.java
@@ -2,7 +2,19 @@ package ca.mcmaster.cas.se2aa4.a2.island.Features.Biomes;
 
 import ca.mcmaster.cas.se2aa4.a2.island.Containers.Island;
 
+import java.util.List;
+import java.util.Map;
+
 public class AmericaProfile extends WhittakerUtil {
 
 
+    @Override
+    void setBiomesBoundaries() {
+
+    }
+
+    @Override
+    Map<String, Map<String, Double>> getBiomesBoundaries() {
+        return null;
+    }
 }
--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Biomes/WhittakerUtil.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Biomes/WhittakerUtil.java
@@ -2,10 +2,30 @@ package ca.mcmaster.cas.se2aa4.a2.island.Features.Biomes;
 
 import ca.mcmaster.cas.se2aa4.a2.island.Containers.Island;
 
+import java.util.List;
+import java.util.Map;
+
 public abstract class WhittakerUtil implements DiagramProfile {
 
+    Island island;
     public void process(Island island) {
+        this.island = island;
+        setBiomesBoundaries();
+
+        // Iterate through each land tile
+
+        // Gather the moisture and elevation data
+
+
 
     }
+
+    // For each profile, set the boundaries for the biomes
+    abstract void setBiomesBoundaries();
+
+    // Gather the biomes boundaries
+    // Stored in this format: {Desert -> {minElevation -> min, maxElevation -> max, minMoisture -> min, maxMoisture -> max}
+    abstract Map<String, Map<String, Double>> getBiomesBoundaries();
+
 
 }
