d66a01a9cd4b321aa5f0530a877893f2599eccb6
Hady Ibrahim <hadyib007@gmail.com>
2023-03-24 13:39:59 -0400

    WIP: lake tests

---
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Water/LakeGeneratorTest.java
@@ -0,0 +1,69 @@
+package ca.mcmaster.cas.se2aa4.a2.island.Features.Water;
+
+import Geometries.Polygon;
+import Geometries.Segment;
+import Geometries.Vertex;
+import ca.mcmaster.cas.se2aa4.a2.island.Features.Biomes.AmericaProfile;
+import ca.mcmaster.cas.se2aa4.a2.island.Geography.Border;
+import ca.mcmaster.cas.se2aa4.a2.island.Geography.Tile;
+import ca.mcmaster.cas.se2aa4.a2.island.Geography.VertexDecorator;
+import ca.mcmaster.cas.se2aa4.a2.island.Island.Island;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LakeGeneratorTest {
+    static Tile tile;
+    static Island island;
+
+    @BeforeAll
+    public static void setup() {
+        //Context
+        Vertex v1 = new Vertex(0.0,0.0);
+        Vertex v2 = new Vertex(0.0,10.0);
+        Vertex v3 = new Vertex(10.0,10.0);
+        Vertex v4 = new Vertex(10.0,0.0);
+        List<Segment> segmentList = new ArrayList<>();
+        segmentList.add(new Segment(v1, v2));
+        segmentList.add(new Segment(v2, v3));
+        segmentList.add(new Segment(v3, v4));
+        segmentList.add(new Segment(v1, v3));
+        Polygon mockPolygon = new Polygon(segmentList);
+
+        List<VertexDecorator> vertices = new ArrayList<>();
+        vertices.add(VertexDecorator.newBuilder().addVertex(v1).build());
+        vertices.add(VertexDecorator.newBuilder().addVertex(v2).build());
+        vertices.add(VertexDecorator.newBuilder().addVertex(v3).build());
+        vertices.add(VertexDecorator.newBuilder().addVertex(v4).build());
+
+        List<Border> borders = new ArrayList<>();
+        borders.add(Border.newBuilder().addV1(vertices.get(0)).addV2(vertices.get(0)).addSegment(new Segment(v1,  v2)).build());
+        List<Border> mockBorder = new ArrayList<>();
+        mockBorder.add(borders.get(0));
+
+        VertexDecorator mockCentroid = VertexDecorator.newBuilder().addVertex(new Vertex(5.0,5.0)).build();
+        vertices.add(mockCentroid);
+
+        tile = Tile.newBuilder().addPolygon(mockPolygon).addBorders(mockBorder).addCentroid(mockCentroid).build();
+        island = new Island();
+        List<Tile> tiles = new ArrayList<>();
+        tiles.add(tile);
+        island.register(vertices, new ArrayList<>(), tiles);
+    }
+
+    @Test
+    public void GenerateLakeTest() {
+        new LakeGenerator().process(island, 1);
+        assertTrue(tile.hasLake());
+    }
+
+    @Test
+    public void GenerateNoLakeTest() {
+        new LakeGenerator().process(island, 0);
+        assertFalse(tile.hasLake());
+    }
+}
