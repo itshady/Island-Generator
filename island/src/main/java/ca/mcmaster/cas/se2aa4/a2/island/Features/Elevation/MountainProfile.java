6a30a53f257c5c2fe128e3b8d6650b106fd19506
richardl2003 <richardliy03@gmail.com>
2023-03-25 12:52:54 -0400

    Added documentation to Elevation package for Features

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Elevation/MountainProfile.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Elevation/MountainProfile.java
@@ -5,6 +5,11 @@ import ca.mcmaster.cas.se2aa4.a2.island.Geography.VertexDecorator;
 
 public class MountainProfile extends ElevationUtil {
 
+    /**
+     * Implementation of calculateAltitude for a Mountain
+     * @param vertex, VertexDecorator
+     * @return Integer, represents the altitude level
+     */
     protected Integer calculateAltitude(VertexDecorator vertex) {
         Double distanceFromCenter = getDistance(center,vertex.getVertex().getCoordinate());
         Double slope = -300/(0.7* Math.min(island.width(), island.height()));
