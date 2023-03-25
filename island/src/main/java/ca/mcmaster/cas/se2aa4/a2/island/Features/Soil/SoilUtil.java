aa501bf4ece27b1635a4813e78c464c96b3cce34
richardl2003 <richardliy03@gmail.com>
2023-03-25 13:22:25 -0400

    Added documentation for Soil package for Shapes

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Soil/SoilUtil.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Soil/SoilUtil.java
@@ -7,6 +7,9 @@ import ca.mcmaster.cas.se2aa4.a2.island.Geography.Tile;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains methods that all profiles for Soil requires to process
+ */
 public abstract class SoilUtil implements SoilProfile {
 
     Island island;
@@ -16,12 +19,20 @@ public abstract class SoilUtil implements SoilProfile {
     // min for land tiles
     private Double minAbsorption = Double.MAX_VALUE;
 
+    /**
+     * Takes a set of tiles and associates an absorption value for each tile
+     * @param island: container with a set of tiles, borders, and VertexDecorators
+     */
     public void process(Island island) {
         this.island = island;
         setSoilProfiles(island);
         standardizeAbsorbances(island);
     }
 
+    /**
+     * Iterates through all Tiles and sets absorption level based on the profile
+     * @param island
+     */
     private void setSoilProfiles(Island island) {
         for (Tile tile : island.getTiles()) {
             if (tile.isOcean()) tile.setAbsorption(0.0);
@@ -47,6 +58,11 @@ public abstract class SoilUtil implements SoilProfile {
     public abstract SoilProfile getSoilProfile();
 
 
+    /**
+     * Based on the type of tile (lake, river, aquifer), an absorption value will be associated with it
+     * @param tile, a tile on the Island
+     * @return Double, value of absorption
+     */
     private Double calculateAbsorption(Tile tile) {
         double absorption = 0.0;
 
@@ -74,10 +90,28 @@ public abstract class SoilUtil implements SoilProfile {
         return absorption;
     }
 
+    /**
+     * Method used to determine the absorption level for a land water element (lake, aquifer)
+     * @param currentTile, tile on the land
+     * @param distance, proximity from a land water element
+     * @return double, value of absorption
+     */
     protected abstract double calcLandWaterAbsorption(Tile currentTile, double distance);
 
+    /**
+     * Method used to determine the absorption level for a river element
+     * @param distance, proximity to a river
+     * @param riverMultiplicity, the multiplicity value for a river
+     * @param riverMoisture, moisture value associated with a river
+     * @return
+     */
     protected abstract double calcRiverAbsorption(double distance, Integer riverMultiplicity, Integer riverMoisture);
 
+    /**
+     * Checks if a given land tile contains a border that is a river
+     * @param tile, a land tile
+     * @return boolean
+     */
     protected boolean touchesRiver(Tile tile) {
         List<Tile> neighbours = new ArrayList<>(tile.getNeighbours().stream()
                 .map(id -> island.getTile(id)).toList());
@@ -89,7 +123,12 @@ public abstract class SoilUtil implements SoilProfile {
         return false;
     }
 
-    // calculate distance between two centroids
+    /**
+     * Gets the distance between the centroids of two tiles
+     * @param sourceTile, tile 1
+     * @param endTile, tile 2
+     * @return double, distance between two tiles
+     */
     protected double calculateDistance(Tile sourceTile, Tile endTile) {
         double sourceX = sourceTile.getCentroid().getX();
         double sourceY = sourceTile.getCentroid().getY();
@@ -100,6 +139,12 @@ public abstract class SoilUtil implements SoilProfile {
         return Math.sqrt(Math.pow(xDifference,2) + Math.pow(yDifference,2));
     }
 
+    /**
+     * Gets the distance between a tile and a border
+     * @param sourceTile, land tile
+     * @param endBorder, border
+     * @return
+     */
     protected double calculateDistance(Tile sourceTile, Border endBorder) {
         double sourceX = sourceTile.getCentroid().getX();
         double sourceY = sourceTile.getCentroid().getY();
@@ -114,6 +159,10 @@ public abstract class SoilUtil implements SoilProfile {
         return Math.sqrt(Math.pow(xDifference,2) + Math.pow(yDifference,2));
     }
 
+    /**
+     * Standardizes absorption values to be between 0 and 100
+     * @param island, container for tiles, borders, and vertex decorators
+     */
     protected void standardizeAbsorbances(Island island) {
         for (Tile tile : island.getTiles()) {
             if (tile.isOcean()) tile.setAbsorption(0.0);
