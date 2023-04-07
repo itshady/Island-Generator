aad2459848c633c81ffb014b6bfd1ccd0c1a971f
Hady Ibrahim <hadyib007@gmail.com>
2023-04-07 15:16:13 -0400

    #13 Add different types of cities

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Cities/City/Capitol.java
@@ -0,0 +1,7 @@
+package ca.mcmaster.cas.se2aa4.a2.island.Features.Cities.City;
+
+public class Capitol extends City {
+    public Capitol() {
+        super(9);
+    }
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a2/island/Features/Cities/City/Village.java
@@ -0,0 +1,7 @@
+package ca.mcmaster.cas.se2aa4.a2.island.Features.Cities.City;
+
+public class Village extends City {
+    public Village() {
+        super(5);
+    }
+}
